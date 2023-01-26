1aec25be407611a800626b064d9e8e26165012a5
Josh1N <jayngwarai@gmail.com>
2023-01-26 09:39:42 +0200

    Added the notify button and update the alert component

--- src/app/products.ts
+++ src/app/products.ts
@@ -9,23 +9,22 @@ export const products = [
   {
     id: 1,
     name: 'Phone XL',
-    price: 799,
-    description: 'A large phone with one of the best screens'
+    price: 999,
+    description: 'A large phone with one of the best screens',
   },
   {
     id: 2,
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
   },
   {
     id: 3,
     name: 'Phone Standard',
-    price: 299,
-    description: 'A mid range phone with great battery power'
-  }
+    price: 899,
+    description: 'A mid range phone with great battery power',
+  },
 ];
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
